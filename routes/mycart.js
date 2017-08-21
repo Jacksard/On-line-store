8b882a4ae2884abdcdee91d03972f354bf5f4b84
jacobgoskha <sardaukarkk@gmail.com>
2017-08-21 17:18:57 -0400

    Added a new object to contain the selected product to be saved inside mycart, by doing so, new id is created. making it possible to add the same product more than once

--- routes/mycart.js
+++ routes/mycart.js
@@ -8,7 +8,8 @@ var ObjectId = mongojs.ObjectId;
 router.get('/', function(req, res, next){
     
     db.mycart.find(function(err, products){
-
+        
+        // Shows 2 decimels after teh point
         var total = 0;
         var sum = 0;
         products.forEach(function(price) {
@@ -28,13 +29,26 @@ router.get('/', function(req, res, next){
 });
 });
 
+// Grabing the product by its Id and adding to mu cart.
+
 router.get('/:id', function(req, res, next){
     var productId = req.params.id;
     console.log(productId);
     db.products.findOne({_id: mongojs.ObjectId(productId)}, function(err, doc) {
     console.log(doc);
+
+    /*pushing the doc into an new object.
+    to make it possible to add the same product more than once, 
+    else the product is saved with the same id, new object creates new id. */
+
+    newP = {};
+    newP.product_name = doc.product_name;
+    newP.price = Number(doc.price);
+    newP.category = doc.category;
+    newP.in_stock = doc.in_stock;
+    newP.desc = doc.desc;
     
-    db.mycart.save(doc, function(err, docs){
+    db.mycart.save(newP, function(err, docs){
             res.status(301).redirect('/')
             });
 
@@ -42,6 +56,7 @@ router.get('/:id', function(req, res, next){
 
 });
 
+// Remove the product from mycart
 
 router.get('/remove/:id', function(req, res, next){
     var removeId = req.params.id;
